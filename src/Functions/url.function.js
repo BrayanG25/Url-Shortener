fcd17a063e42ca7b8d9e8275e27a824d787cbe5e
Brayan <briang0325@gmail.com>
2024-03-09 21:53:49 -0500

    Edit patron de url para que acepte parametros dentro de la consulta

--- src/Functions/url.function.js
+++ src/Functions/url.function.js
@@ -1,4 +1,4 @@
 export const isValidUrl = (url) => {
-    const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
+    const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*(\?.*)?$/;
     return urlPattern.test(url);
 }
